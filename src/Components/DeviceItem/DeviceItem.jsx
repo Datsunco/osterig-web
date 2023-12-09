efb233815f0995e812a81c8a038a60f6cc730b8d
DragfaR <mc.dragfar.228@gmail.com>
2023-12-09 23:19:03 +0300

    yaYebalsaPerviyRaz

--- src/Components/DeviceItem/DeviceItem.jsx
+++ src/Components/DeviceItem/DeviceItem.jsx
@@ -43,7 +43,17 @@ const DeviceItem = ({ device }) => {
                         onClick={() => favorites.addFavorite(device) && toggleFavorite()}
                         alt=""
                         src="/heart.svg" />
+                        
                 }
+                <button onClick={() => cart.addDevice(device)} class="busket1" href=''>В корзину</button>
+                <button onClick={() => cart.addDevice(device)} class="busketUzhe" href=''>Уже в корзине
+                    <div class="svgGalka">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+                        <path d="M13.2 6L7.42499 12L4.42499 9" stroke="#0071E3" stroke-width="1.5"/>
+                        </svg>
+                    </div>
+                </button>
+                <button onClick={() => cart.addDevice(device)} class="busketNetu" href=''>Нет в наличии</button>
                 <Card
                     style={{ width: 150, cursor: 'pointer', margin:'auto'}}
                     border={"light"}
@@ -62,7 +72,6 @@ const DeviceItem = ({ device }) => {
                 </div>
                 <div class="text13">{device.price || device.currencyPrice || device?.productPriceList?.[0].currencyPrice}
                 </div>
-                <button onClick={() => toggleFavorite()} class="busket1" href=''>В корзину</button>
             </div>
         </div>
     );
