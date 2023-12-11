8e40b96edf22fa85b39c5f5b14f331628b35443e
DatsunCo <danya200375d@gmail.com>
2023-12-11 14:55:04 +0300

    deviceItem create states for add to cart button

--- src/Components/DeviceItem/DeviceItem.jsx
+++ src/Components/DeviceItem/DeviceItem.jsx
@@ -15,17 +15,26 @@ const DeviceItem = ({ device }) => {
     const toggleFavorite = () => {
         setIsFavorite(!isFavorite); // Инвертируем текущее состояние избранного
     };
+
     useEffect(() => {
         favorites.favorites.forEach(fav => {
-            if (device.productCode == fav.productCode)
+            if (device.productCode == fav.productCode) {
+                // console.log(device.productModel, fav.productCode)
                 setIsFavorite(true)
+            }
         });
         cart.devices.forEach(dev => {
-            if (device.productModel == dev.productCode)
-                setIsCart(!isCart)
+            if (device.productCode == dev.productCode){
+                // console.log(device.productModel, dev.productCode)
+                setIsCart(true)
+            }
         });
 
-    }, [favorites, cart, device, isFavorite, setIsCart, setIsFavorite])
+        return () => {
+            setIsFavorite(false)
+            setIsCart(false)
+        }
+    }, [favorites.favorites, cart.devices, device])
 
     return (
         <div className={"mt-3"}>
@@ -42,19 +51,11 @@ const DeviceItem = ({ device }) => {
                         onClick={() => favorites.addFavorite(device) && toggleFavorite()}
                         alt=""
                         src="/heart.svg" />
-                        
+
                 }
-                <button onClick={() => cart.addDevice(device)} class="busket1" href=''>В корзину</button>
-                <button onClick={() => cart.addDevice(device)} class="busketUzhe" href=''>Уже в корзине
-                    <div class="svgGalka">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
-                        <path d="M13.2 6L7.42499 12L4.42499 9" stroke="#0071E3" stroke-width="1.5"/>
-                        </svg>
-                    </div>
-                </button>
-                <button onClick={() => cart.addDevice(device)} class="busketNetu" href=''>Нет в наличии</button>
+                <DataComponent device={device} cartState={isCart}/>
                 <Card
-                    style={{ width: 150, cursor: 'pointer', margin:'auto'}}
+                    style={{ width: 150, cursor: 'pointer', margin: 'auto' }}
                     border={"light"}
                     class="card"
                     onClick={() => navigate(DEVICE_ROUTE + '/' + device.productCode, { state: { productCode: device.productCode } })}>
@@ -74,6 +75,35 @@ const DeviceItem = ({ device }) => {
             </div>
         </div>
     );
+};
+
+const DataComponent = ({ device, cartState }) => {
+    const { cart } = useContext(Context)
+    const [isCart, setIsCart] = useState(cartState)
+
+    useEffect(() => {
+        setIsCart(cartState)
+        // console.log(device, cartState)
+    }, [cartState])
+
+    if (device?.domesticStockVO?.total == 0 || device?.stockNumber == 0)
+        return (
+            <button class="busketNetu" href=''>Нет в наличии</button>
+        );
+    if (isCart)
+        return (
+            <button class="busketUzhe" href=''>Уже в корзине
+                <div class="svgGalka">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+                        <path d="M13.2 6L7.42499 12L4.42499 9" stroke="#0071E3" stroke-width="1.5" />
+                    </svg>
+                </div>
+            </button>
+        )
+    else
+        return (
+            <button onClick={() => cart.addDevice(device) && setIsCart(true)} class="busket1" href=''>В корзину</button>
+        )
 };
 
 export default observer(DeviceItem);
