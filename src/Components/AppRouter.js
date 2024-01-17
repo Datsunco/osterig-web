34a3fffa243aabd5e1c70b79578295bc1b538893
DatsunCo <danya200375d@gmail.com>
2024-01-17 16:56:50 +0300

    mainpage navigate from wherever

--- src/Components/AppRouter.js
+++ src/Components/AppRouter.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react';
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, Navigate} from 'react-router-dom';
 import { publicRoutes } from '../routes';
 import { Context } from '..';
 import Orders from './Orders/Orders';
@@ -13,7 +13,9 @@ const AppRouter = () => {
             {publicRoutes.map(({path, Component}) =>
                 <Route key={path} path={path} element={<Component/>}/>
             )}
+            <Route path="*" element={<Navigate to="/mainpage" />} />
         </Routes>
+        
     );
 };
 
