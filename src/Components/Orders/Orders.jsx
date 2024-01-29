9be3dc840e5d55e28a8bbdfebec577cd795823ae
DatsunCo <danya200375d@gmail.com>
2024-01-29 21:23:35 +0300

    fix: orders page logic

--- src/Components/Orders/Orders.jsx
+++ src/Components/Orders/Orders.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext} from 'react';
+import React, { useEffect, useContext } from 'react';
 import styles from './Orders.css';
 import Header from '../Header/Header'
 import BottomMenu from '../BottomMenu/BottomMenu';
@@ -9,7 +9,6 @@ const Orders = () => {
     const { orders } = useContext(Context)
 
     useEffect(() => {
-        console.log('render')
         if (localStorage.getItem('token')) {
             orders.getOrders()
         }
@@ -22,24 +21,26 @@ const Orders = () => {
                     <h1>ЗАКАЗЫ</h1>
                 </div>
                 <div className='frame_for_orders_cart'>
-                    {orders.orders.map(order => 
-                        <div className='orders_cart'>
-                        <div className='orders_number'>№{order.order}</div>
-                        <div className='orders_status_svg'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 6 6" fill="none">
-                                <circle cx="3" cy="3" r="3" fill="#7DF81D" />
-                            </svg>
-                        </div>
-                        <div className='orders_status'>Доставляется</div>
-                        <div className='orders_date'>10 янв 2023</div>
-                        <div className='orders_images_block'>
-                            <img src='https://assets.lcsc.com/images/lcsc/900x900/20180914_Infineon-Technologies-BTS6143D_C95232_front_10.jpg' className='orders_images' />
-                            <img src='https://assets.lcsc.com/images/lcsc/900x900/20180914_Infineon-Technologies-BTS6143D_C95232_front_10.jpg' className='orders_images' />
-                            <div className='orders_if_images_so_much'>+2</div>
-                        </div>
-                        <div className='orders_price'>110 024 ₽</div>
-                    </div>
-                    <div className={'orders_line'+ (i == orders.orders.length ? 'last' : '')}></div>
+                    {orders.orders.map((order,index) =>
+                        <>
+                            <div className='orders_cart'>
+                                <div className='orders_number'>№{order.order}</div>
+                                <div className='orders_status_svg'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 6 6" fill="none">
+                                        <circle cx="3" cy="3" r="3" fill="#7DF81D" />
+                                    </svg>
+                                </div>
+                                <div className='orders_status'>Доставляется</div>
+                                <div className='orders_date'>10 янв 2023</div>
+                                <div className='orders_images_block'>
+                                    <img src='https://assets.lcsc.com/images/lcsc/900x900/20180914_Infineon-Technologies-BTS6143D_C95232_front_10.jpg' className='orders_images' />
+                                    <img src='https://assets.lcsc.com/images/lcsc/900x900/20180914_Infineon-Technologies-BTS6143D_C95232_front_10.jpg' className='orders_images' />
+                                    <div className='orders_if_images_so_much'>+2</div>
+                                </div>
+                                <div className='orders_price'>110 024 ₽</div>
+                            </div>
+                            <div className={'orders_line' + (index == orders.orders.length ? 'last' : '')}></div>
+                        </>
                     )}
                 </div>
             </div>
