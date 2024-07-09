b91c43f66041bc343a03145eff5de88d5b5063e6
DatsunCo <danya200375d@gmail.com>
2024-07-09 12:18:48 +0300

    fixed

--- src/Components/Orders/Orders.jsx
+++ src/Components/Orders/Orders.jsx
@@ -26,7 +26,7 @@ const Orders = () => {
 
     useEffect(() => {
         if (id) {
-           sendConfirm(id)
+            sendConfirm(id)
         }
     }, [])
 
@@ -37,29 +37,56 @@ const Orders = () => {
                 <div className='text_orders_h1'>
                     <h1>ЗАКАЗЫ</h1>
                 </div>
-                <div className='frame_for_orders_cart'>
-                    {orders.orders.map((order,index) =>
-                        <>
-                            <div className='orders_cart'>
-                                <div className='orders_number'>№{order.order}</div>
-                                <div className='orders_status_svg'>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 6 6" fill="none">
-                                        <circle cx="3" cy="3" r="3" fill="#7DF81D" />
-                                    </svg>
-                                </div>
-                                <div className='orders_status'>Доставляется</div>
-                                <div className='orders_date'>10 янв 2023</div>
-                                <div className='orders_images_block'>
-                                    <img src='https://assets.lcsc.com/images/lcsc/900x900/20180914_Infineon-Technologies-BTS6143D_C95232_front_10.jpg' className='orders_images' />
-                                    <img src='https://assets.lcsc.com/images/lcsc/900x900/20180914_Infineon-Technologies-BTS6143D_C95232_front_10.jpg' className='orders_images' />
-                                    <div className='orders_if_images_so_much'>+2</div>
-                                </div>
-                                <div className='orders_price'>110 024 ₽</div>
-                            </div>
-                            <div className={'orders_line' + (index == orders.orders.length ? 'last' : '')}></div>
-                        </>
-                    )}
-                </div>
+                {!localStorage.getItem('token') ? (
+                    <>
+                        <h3 className='cart_elements_empty_main_text'>ЧТОБЫ ПОЛУЧИТЬ ДОСТУП К КОРЗИНЕ, СНАЧАЛА АВТОРИЗИРУЙТЕСЬ</h3>
+                        <h4 className='cart_elements_empty_secondary_text'>В каталоге можно найти много товаров</h4>
+                        <button onClick={() => console.log('check')} className='cart_elements_empty_button'>В каталог</button>
+                    </>
+                )
+                    :
+                    (
+                        <div className='frame_for_orders_cart'>
+                            {orders.orders.length != 0 ?
+                                (
+                                    <>
+                                        {orders.orders.map((order, index) =>
+                                            <>
+                                                <div className='orders_cart'>
+                                                    <div className='orders_number'>№{order.order}</div>
+                                                    <div className='orders_status_svg'>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 6 6" fill="none">
+                                                            <circle cx="3" cy="3" r="3" fill="#7DF81D" />
+                                                        </svg>
+                                                    </div>
+                                                    <div className='orders_status'>Доставляется</div>
+                                                    <div className='orders_date'>10 янв 2023</div>
+                                                    <div className='orders_images_block'>
+                                                        <img src='https://assets.lcsc.com/images/lcsc/900x900/20180914_Infineon-Technologies-BTS6143D_C95232_front_10.jpg' className='orders_images' />
+                                                        <img src='https://assets.lcsc.com/images/lcsc/900x900/20180914_Infineon-Technologies-BTS6143D_C95232_front_10.jpg' className='orders_images' />
+                                                        <div className='orders_if_images_so_much'>+2</div>
+                                                    </div>
+                                                    <div className='orders_price'>110 024 ₽</div>
+                                                </div>
+                                                <div className={'orders_line' + (index == orders.orders.length ? 'last' : '')}></div>
+                                            </>
+                                        )}
+                                    </>
+                                )
+                                :
+                                (
+                                    <>
+                                        <h3 className='cart_elements_empty_main_text'>У ВАС ПОКА ЧТО НЕТ АКТИВНЫХ ЗАКАЗАОВ, САМОЕ ВРЕМЯ СДЕЛАТЬ ЗАКАЗ</h3>
+                                        <h4 className='cart_elements_empty_secondary_text'>В каталоге можно найти много товаров</h4>
+                                        <button onClick={() => console.log('check')} className='cart_elements_empty_button'>В каталог</button>
+                                    </>
+                                )
+                            }
+
+                        </div>
+                    )
+                }
+
             </div>
             <BottomMenu />
         </div>
